4b1f8a975d6d5856109a53e698414d4f0b013cb4
FinFin <FinFin1351@gmail.com>
2025-12-16 18:53:35 +0900

    feat: increase maximum message size limit from 5MB to 500MB

--- handlers.go
+++ handlers.go
@@ -285,7 +285,7 @@ const (
 	pingPeriod = pongWait / 2
 
 	// Maximum message size allowed from peer.
-	maxMessageSize = 5242880
+	maxMessageSize = 524288000
 )
 
 // TODO: consider moving these to Server as config params
